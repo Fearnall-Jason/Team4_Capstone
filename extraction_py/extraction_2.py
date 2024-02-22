5a05afb34c2aee8ecca45fd579aa825c7bf6d350
ImMaxRoss <max@maxross.com>
2024-02-22 12:50:32 -0500

    adding lambda extraction py folder with all lambda scripts used

---
+++ extraction_py/extraction_2.py
@@ -0,0 +1,25 @@
+import json
+import boto3
+import pandas as pd
+import requests
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    bucket ='semistructuredata'
+    url = 'https://data.cityofnewyork.us/resource/kpav-sd4t.json'
+    
+    params = {'$limit': 1000, '$offset':1000}
+    response = requests.get(url,params=params)
+    data = response.json()
+
+
+    df = pd.DataFrame(data)
+    df.drop_duplicates(inplace=True)
+    
+    
+    csv_data = df.to_csv(index=False)
+    fileName = 'data2.csv'
+    uploadByteStream = bytes(csv_data.encode('utf-8'))
+    s3.put_object(Bucket=bucket, Key=fileName, Body=uploadByteStream)
+    print('Put Complete')
